c09ec194a4960152ed35b09bf03ec201485cd0bb
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-07-08 14:46:07 -0700

    add a basic example of passing a future through roc

---
+++ future-passthrough/Effect.roc
@@ -0,0 +1,8 @@
+hosted Effect
+    exposes [Effect, after, map, always, forever, loop, sleep]
+    imports []
+    generates Effect with [after, map, always, forever, loop]
+
+Future := Nat
+
+sleep : Effect Future
---
+++ future-passthrough/src/lib.rs
@@ -0,0 +1,138 @@
+#![allow(non_snake_case)]
+
+use core::alloc::Layout;
+use core::ffi::c_void;
+use core::mem::MaybeUninit;
+use libc;
+use std::ffi::CStr;
+use std::pin::Pin;
+use std::os::raw::c_char;
+use tokio::runtime::Runtime;
+use tokio::time::Sleep;
+use std::time::Instant;
+
+extern "C" {
+    #[link_name = "roc__mainForHost_1_exposed_generic"]
+    fn roc_main(output: *mut u8);
+
+    #[link_name = "roc__mainForHost_size"]
+    fn roc_main_size() -> i64;
+
+    #[link_name = "roc__mainForHost_1_Fx_caller"]
+    fn call_Fx(flags: *const u8, closure_data: *const u8, output: *mut u8);
+
+    #[allow(dead_code)]
+    #[link_name = "roc__mainForHost_1_Fx_size"]
+    fn size_Fx() -> i64;
+
+    #[link_name = "roc__mainForHost_1_Fx_result_size"]
+    fn size_Fx_result() -> i64;
+}
+
+static mut RT: MaybeUninit<Runtime> = MaybeUninit::uninit();
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
+    libc::malloc(size)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_realloc(
+    c_ptr: *mut c_void,
+    new_size: usize,
+    _old_size: usize,
+    _alignment: u32,
+) -> *mut c_void {
+    libc::realloc(c_ptr, new_size)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
+    libc::free(c_ptr)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_panic(c_ptr: *mut c_void, tag_id: u32) {
+    match tag_id {
+        0 => {
+            let slice = CStr::from_ptr(c_ptr as *const c_char);
+            let string = slice.to_str().unwrap();
+            eprintln!("Roc hit a panic: {}", string);
+            std::process::exit(1);
+        }
+        _ => todo!(),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_memcpy(dst: *mut c_void, src: *mut c_void, n: usize) -> *mut c_void {
+    libc::memcpy(dst, src, n)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_memset(dst: *mut c_void, c: i32, n: usize) -> *mut c_void {
+    libc::memset(dst, c, n)
+}
+
+fn run_roc_main() -> Box<Sleep> {
+    let size = unsafe { roc_main_size() } as usize;
+    let layout = Layout::array::<u8>(size).unwrap();
+
+    unsafe {
+        // TODO allocate on the stack if it's under a certain size
+        let buffer = std::alloc::alloc(layout);
+
+        roc_main(buffer);
+
+        let result = call_the_closure(buffer);
+
+        std::alloc::dealloc(buffer, layout);
+
+        result
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn rust_main() -> i32 {
+    unsafe {
+        RT = MaybeUninit::new(
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap(),
+        );
+        RT.assume_init_ref().block_on(async {
+            println!("Starting 1 second async sleep...");
+            let start = Instant::now();
+            Pin::from(run_roc_main()).await;
+            let elapsed_time = start.elapsed().as_millis();
+            println!("Roc main took {}ms", elapsed_time);
+        });
+    }
+    // Exit code
+    0
+}
+
+unsafe fn call_the_closure(closure_data_ptr: *const u8) -> Box<Sleep> {
+    let size = size_Fx_result() as usize;
+    let layout = Layout::array::<u8>(size).unwrap();
+    let buffer = std::alloc::alloc(layout) as *mut u8;
+
+    call_Fx(
+        // This flags pointer will never get dereferenced
+        MaybeUninit::uninit().as_ptr(),
+        closure_data_ptr as *const u8,
+        buffer as *mut u8,
+    );
+
+    let out = Box::from_raw(*(buffer as *mut *mut Sleep));
+    std::alloc::dealloc(buffer as *mut u8, layout);
+
+    out
+}
+
+#[no_mangle]
+pub extern "C" fn roc_fx_sleep() -> *mut Sleep {
+    use tokio::time::{sleep, Duration};
+    Box::into_raw(Box::new(sleep(Duration::from_millis(1000))))
+}
