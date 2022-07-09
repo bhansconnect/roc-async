3cec0cdd8337d506bd9dfec265494b093c9806a0
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-07-08 21:01:43 -0700

    get explicit continuation working

--- 01-explicit-continuations/src/lib.rs
+++ 01-explicit-continuations/src/lib.rs
@@ -2,28 +2,29 @@
 
 use core::alloc::Layout;
 use core::ffi::c_void;
-use core::mem::MaybeUninit;
 use libc;
 use std::ffi::CStr;
 use std::os::raw::c_char;
-use std::pin::Pin;
 
 extern "C" {
     #[link_name = "roc__mainForHost_1_exposed_generic"]
-    fn roc_main(output: *mut u8);
+    fn roc_main(output: *mut u8, x: i32);
 
     #[link_name = "roc__mainForHost_size"]
     fn roc_main_size() -> i64;
 
     #[link_name = "roc__mainForHost_1_Main_caller"]
-    fn call_Fx(flags: *const u8, closure_data: *const u8, output: *mut u8);
+    fn call_Main(flags: *const i32, closure_data: *const u8, output: *mut u8);
 
     #[allow(dead_code)]
     #[link_name = "roc__mainForHost_1_Main_size"]
-    fn size_Fx() -> i64;
+    fn size_Main() -> i64;
 
     #[link_name = "roc__mainForHost_1_Main_result_size"]
-    fn size_Fx_result() -> i64;
+    fn size_Main_result() -> i64;
+
+    #[link_name = "roc__mainForHost_1_Continuation_caller"]
+    fn call_Continuation(flags: *const i32, closure_data: *const u8, output: *mut i32);
 }
 
 #[no_mangle]
@@ -74,35 +75,63 @@ pub extern "C" fn rust_main() -> i32 {
     let size = unsafe { roc_main_size() } as usize;
     let layout = Layout::array::<u8>(size).unwrap();
 
+    let x = 21;
+    let y = 15;
+    let z = -9;
     unsafe {
         // TODO allocate on the stack if it's under a certain size
         let buffer = std::alloc::alloc(layout);
 
-        roc_main(buffer);
+        roc_main(buffer, x);
 
-        let result = call_the_closure(buffer);
-
+        let (cont_buffer, cont_layout) = call_main_closure(buffer, y);
         std::alloc::dealloc(buffer, layout);
 
-        result
+        let out = call_continuation_closure(cont_buffer, z);
+        std::alloc::dealloc(cont_buffer, cont_layout);
+
+        println!("x = {}, y = {}, z = {}", x, y, z);
+        println!("(x - 1) + (y + x - 1) + x + y + z = ???");
+        println!(
+            "{} + {} + {} + {} + {} = {}",
+            x - 1,
+            y + x - 1,
+            x,
+            y,
+            z,
+            out
+        );
     };
     // Exit code
     0
 }
 
-unsafe fn call_the_closure(closure_data_ptr: *const u8) -> i32 {
-    let size = size_Fx_result() as usize;
+unsafe fn call_main_closure(closure_data_ptr: *const u8, y: i32) -> (*mut u8, Layout) {
+    let size = size_Main_result() as usize;
     let layout = Layout::array::<u8>(size).unwrap();
     let buffer = std::alloc::alloc(layout) as *mut u8;
 
-    call_Fx(
+    // call_Main expects y to be stored in the flags.
+    call_Main(
         // This flags pointer will never get dereferenced
-        MaybeUninit::uninit().as_ptr(),
-        closure_data_ptr as *const u8,
-        buffer as *mut u8,
+        &y as *const i32,
+        closure_data_ptr,
+        buffer,
     );
 
-    std::alloc::dealloc(buffer as *mut u8, layout);
+    (buffer, layout)
+}
 
-    0
+unsafe fn call_continuation_closure(closure_data_ptr: *const u8, z: i32) -> i32 {
+    let mut out: i32 = 0;
+
+    // call_Continuation expects z to be stored in the flags.
+    call_Continuation(
+        // This flags pointer will never get dereferenced
+        &z as *const i32,
+        closure_data_ptr,
+        &mut out as *mut i32,
+    );
+
+    out
 }
